615a717fe124381c85d824433e357263899f88f9
Albert Sison <asison89@gmail.com>
2015-02-18 00:10:17 -0800

    Fix cart table

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+         has_many :carts
+         has_many :line_items, through: :carts
+         has_many :products, through: :line_items
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150218064150) do
+ActiveRecord::Schema.define(version: 20150218080826) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "line_items", force: :cascade do |t|
