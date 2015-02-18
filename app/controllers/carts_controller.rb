83c0a89e79beec57252831324622926f87ae033e
Albert Sison <asison89@gmail.com>
2015-02-17 23:40:31 -0800

    Edit devise and cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,6 @@
 class CartsController < ApplicationController
-  before_action :set_cart, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+  # before_action :set_cart, only: [:show, :edit, :update, :destroy]
 
   def index
     @carts = Cart.all
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'welcome/index'
+
   resources :line_items
 
   resources :carts
@@ -6,6 +9,8 @@ Rails.application.routes.draw do
   get 'store/index'
 
   get "/products", to: "products#index"
+
+  root to: "welcome#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
